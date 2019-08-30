2a0738b67350dcf6af6d82a264d84a6d12c8ed22
Pudou <notreal@pudou.io>
2019-08-30 00:33:47 -0300

    Refactoring Tab Nav

--- js/modules/tabnav.js
+++ js/modules/tabnav.js
@@ -1,25 +1,34 @@
-export default function initTabNav() {
-  // Selecting list of menus lists-itens and all content sections
-  const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
-  const tabContent = document.querySelectorAll('[data-tab="content"] section');
-  function activeTab(index) {
-    tabContent.forEach((item) => {
+export default class TabNav {
+  constructor(tabMenuList, tabContentSection) {
+    // Selecting list of menus lists-itens and all content sections
+    this.tabMenu = document.querySelectorAll(tabMenuList);
+    this.tabContent = document.querySelectorAll(tabContentSection);
+    this.activeTab = this.activeTab.bind(this);
+  }
+
+  activeTab(index) {
+    const tab = this.tabContent;
+    tab.forEach((item) => {
       item.classList.remove('active', item.dataset.anime);
     });
-    tabContent[index].classList.add('active', tabContent[index].dataset.anime);
+    tab[index].classList.add('active', tab[index].dataset.anime);
   }
-  // Verifying if our selections exists in this page, avoiding errors here
-  if (tabMenu.length && tabContent.length) {
-  // Activating first content section to avoid empty page on first acess
-    tabContent[0].classList.add('active', tabContent[0].dataset.anime);
-    /* Function to clean all active css classes at each item of the me-
-    nu's list, and, then, adding active class on the clicked menu item ↓
-    Adding event for the clicks on menu with previous
-    function as callback (index required!) */
-    tabMenu.forEach((item, index) => {
+
+  addTabNavEvent() {
+    this.tabMenu.forEach((item, index) => {
       item.addEventListener('click', () => {
-        activeTab(index);
+        this.activeTab(index);
       });
     });
+  }
+
+  activeDatasetAnime(i) {
+    const tab = this.tabContent;
+    if (tab.length && this.tabMenu.length) this.activeTab(i);
+  }
+
+  init() {
+    this.activeDatasetAnime(0);
+    this.addTabNavEvent();
   }
 }
