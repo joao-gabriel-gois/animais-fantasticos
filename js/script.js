1fa6afbdc2e27c7d9107508625858805073121de
stdnt-cl <stdnt-cl@mac.git>
2019-09-10 00:32:07 -0300

    Final Adjustments - Correcting conflicts in modal

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ accordion.init();
 const tabnav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section');
 tabnav.init();
 
-const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="fechar"]');
+const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]');
 modal.init();
 
 const tooltip = new Tooltip('[data-tooltip]');
