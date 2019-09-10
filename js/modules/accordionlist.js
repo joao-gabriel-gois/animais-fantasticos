4497b097a5334e25858fbac7c363be4ad4e2d1a4
stdnt-cl <stdnt-cl@mac.git>
2019-09-09 23:13:08 -0300

    Ending-Adjustments

--- js/modules/accordionlist.js
+++ js/modules/accordionlist.js
@@ -1,21 +1,5 @@
 export default class Accordion {
   constructor(list) {
-<<<<<<< HEAD
-    if (this.accordionList === undefined) {
-      this.accordionList = document.querySelectorAll('[data-anime="accordion"] dt');  
-    } else {
-      this.accordionList = document.querySelectorAll(list);
-    }
-    this.activeClass = 'active';
-    //this.activeAccordion = this.activeAccordion
-  }
-  addAccordionEvent() {
-    // Adding event to each element of accordion
-    this.accordionList.forEach((item) => {
-      item.addEventListener('click', this.toggleAccordion(item));
-    });
-  } // Event's Callback Function that actives css animations
-=======
     this.accordionList = document.querySelectorAll(list);
     this.activeClass = 'active';
   }
@@ -27,21 +11,10 @@ export default class Accordion {
     });
   } // Event's Callback Function that actives css animations
 
->>>>>>> 1f55f27a46c958d36e98d3ed6b7c51e28e30d75b
   toggleAccordion(item) {
     item.classList.toggle(this.activeClass);
     item.nextElementSibling.classList.toggle(this.activeClass);
   }
-<<<<<<< HEAD
-  init() {
-    // Verifying
-    if (this.accordionList.length) {
-      // Activating first "Q" and "F" of the FAQ
-      this.accordionList[0].classList.add(this.activeClass);
-      this.accordionList[0].nextElementSibling.classList.add(this.activeClass);
-      this.addAccordionEvent();
-    }
-=======
 
   init() {
     // Verifying
@@ -51,6 +24,5 @@ export default class Accordion {
       this.addAccordionEvent();
     }
     return this;
->>>>>>> 1f55f27a46c958d36e98d3ed6b7c51e28e30d75b
   }
 }
--- js/script.js
+++ js/script.js
@@ -1,38 +1,18 @@
-<<<<<<< HEAD
-import initTabNav from './modules/tabnav.js';
-=======
 // (1) Importando Modules
 // (1a) console.log('Funções do começo do curso até', idem);
 // ↓↓  const idem = ' Sessão 5 → "efeitos no dom" ';
 import TabNav from './modules/tabnav.js';
->>>>>>> 1f55f27a46c958d36e98d3ed6b7c51e28e30d75b
 import Accordion from './modules/accordionlist.js';
 import SmoothScroll from './modules/smoothscroll.js';
 import initScrollAnimation from './modules/scrollanimation.js';
 // (1b) console.log('A partir daqui importando de' + idem + ' em diante.');
 import Modal from './modules/modal.js';
-import initTooltip from './modules/tooltip.js';
+import Tooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import initFetchAnimais from './modules/fetch-animais.js';
 import initFetchBTC from './modules/fetchingbtc.js';
-<<<<<<< HEAD
-import Tooltip from './modules/tooltip.js';
-// (2) Ativando as Funções importadas
-console.log('teste1');
-const smoothscroll = new SmoothScroll('[data-menu="smoothScroll"] a[href^="#"]');
-smoothscroll.init();
-const accordionList = new Accordion();
-accordionList.init();
-const tooltip = new Tooltip('[data-tooltip]');
-tooltip.init();
-// (2b) console.log('Ativando as Funções antes de' + idem);
-initTabNav();
-initScrollAnimation();
-initModal();
-
-=======
 // (2) Activating imported classes
 
 const smoothscroll = new SmoothScroll('[data-menu="smoothScroll"] a[href^="#"]');
@@ -47,10 +27,11 @@ tabnav.init();
 const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="fechar"]');
 modal.init();
 
+const tooltip = new Tooltip('[data-tooltip]');
+tooltip.init();
+
 // (2b) console.log('Ativando as Funções antes de' + idem);
 initScrollAnimation();
-initTooltip();
->>>>>>> 1f55f27a46c958d36e98d3ed6b7c51e28e30d75b
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
