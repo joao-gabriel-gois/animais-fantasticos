8ac5a7a1418786bd1ec0655ef4da826247868177
stdnt-cl <stdnt-cl@mac.git>
2019-09-11 10:43:35 -0300

    Test

--- js/modules/scrollanimation.js
+++ js/modules/scrollanimation.js
@@ -2,23 +2,37 @@ export default class ScrollAnimation {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
     this.halfWindow = window.innerHeight * 0.57;
-    this.animateScroll = this.animateScroll.bind(this);
+    this.animateAtRightDistance = this.animateAtRightDistance.bind(this);
   }
-  animateScroll() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const isVisibleSection = (sectionTop - this.halfWindow) < 0;
-      if (isVisibleSection) {
-        section.classList.add('active');
-      } else if (section.classList.contains('active')) {
-        section.classList.remove('active');
+  setDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const elementOffsetTop = section.offsetTop;
+      return {
+        element: section,
+        elementOffsetTop: Math.floor(elementOffsetTop - this.halfWindow)
       }
     });
   }
+  animateAtRightDistance(event) {
+    console.log(event);//adding to many events, need to refactor this
+    this.distance.forEach((item) => {
+      const itemHTMLClass = item.element.classList;
+      if (window.pageYOffset > item.elementOffsetTop) {
+        itemHTMLClass.add('active');
+      } else if (itemHTMLClass.contains('active')) {
+        itemHTMLClass.remove('active');
+      }
+    })
+  }
   init() {
     if (this.sections.length) {
-      this.animateScroll();
-      window.addEventListener('scroll', this.animateScroll);
-      }
+      this.setDistance();
+      this.animateAtRightDistance();
+      window.addEventListener('scroll', this.animateAtRightDistance);
+    }
+    return this;
+  }
+  stop() {
+    window.removeEventListener('scroll', this.animateAtRightDistance);
   }
 }
