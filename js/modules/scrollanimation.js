25efb544ff955af90b24e6b8a1ebd9a4e2233031
stdnt-cl <stdnt-cl@mac.git>
2019-09-11 02:08:03 -0300

    Original Animated Section's by Scrolling Down - Refactored

--- js/modules/scrollanimation.js
+++ js/modules/scrollanimation.js
@@ -1,22 +1,24 @@
-export default function initScrollAnimation() {
-// Selection scroll-animated sections
-  const sections = document.querySelectorAll('[data-anime="scroll"]');
-  // Creating Event's Callback function
-  function animaScroll() {
-    sections.forEach((section) => {
-      // Setting the client window point to start scroll animation
-      const isAnimatedPoint = (section.getBoundingClientRect().top - window.innerHeight * 0.57) < 0;
-      // Activating it if it's time to
-      if (isAnimatedPoint) { section.classList.add('active'); } else if (section.classList.contains('active')) {
+export default class ScrollAnimation {
+  constructor(sections) {
+    this.sections = document.querySelectorAll(sections);
+    this.halfWindow = window.innerHeight * 0.57;
+    this.animateScroll = this.animateScroll.bind(this);
+  }
+  animateScroll() {
+    this.sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isVisibleSection = (sectionTop - this.halfWindow) < 0;
+      if (isVisibleSection) {
+        section.classList.add('active');
+      } else if (section.classList.contains('active')) {
         section.classList.remove('active');
       }
     });
   }
-  // Verifying
-  if (sections.length) {
-  // Activating animation of first scroll-animated section
-    sections[0].classList.add('active');
-    // Adding scroll event to window to launch previous callback func.
-    window.addEventListener('scroll', animaScroll);
+  init() {
+    if (this.sections.length) {
+        this.sections[0].classList.add('active');
+        window.addEventListener('scroll', this.animateScroll);
+      }
   }
 }
