0c2bd79d896ea0173504a7026ec4249fff6cd08e
stdnt-cl <stdnt-cl@mac.git>
2019-09-10 22:38:48 -0300

    refactoring animated numbers final

--- js/modules/fetch-animais.js
+++ js/modules/fetch-animais.js
@@ -1,21 +1,26 @@
-import initAnimaNumeros from './anima-numeros.js';
+import AnimatedNumbers from './animated-numbers.js';
 
 export default function initFetchAnimais() {
   function createAnimal(animal) {
     const div = document.createElement('div');
-    div.classList.add('numero-animal');
-    div.innerHTML = `<h3>${animal.species}</h3><span data-numero>${animal.total}</span>`;
+    div.classList.add('number-animal');
+    div.innerHTML = `<h3>${animal.species}</h3><span data-number>${animal.total}</span>`;
     return div;
+  }
+  function initAnimatedNumbers() {
+    const animatedNumbers = new AnimatedNumbers('[data-number]', '.numbers', 'active');
+    animatedNumbers.init();
   }
   async function fetchAnimais(url) {
     const animaisResponse = await fetch(url);
     const animaisJson = await animaisResponse.json();
     animaisJson.forEach((animal) => {
       const divAnimal = createAnimal(animal);
-      const numbersGrid = document.querySelector('.numeros-grid');
+      const numbersGrid = document.querySelector('.numbers-grid');
       numbersGrid.appendChild(divAnimal);
     });
-    initAnimaNumeros();
+    initAnimatedNumbers();
   }
+  
   fetchAnimais('./animaisapi.json');
 }
