ebed67cf5021ee55a84004e7f2f4339c13663d1e
stdnt-cl <stdnt-cl@mac.git>
2019-09-12 18:58:28 -0300

    ending commercial availability refactoring

--- js/modules/commercial-availability.js
+++ js/modules/commercial-availability.js
@@ -2,21 +2,30 @@ export default class CommercialAvailability {
   constructor(element, htmlClass) {
     this.element = document.querySelector(element);
     this.htmlClass = htmlClass;
-
-    const weekDays = this.element.dataset.week.split(',').map(item => +item);
-    const weekHours = this.element.dataset.hour.split(',').map(Number);
+  }
+  setCommercialAvailability() {
+    this.weekDays = this.element.dataset.week.split(',').map(item => +item);
+    this.weekHours = this.element.dataset.hour.split(',').map(Number);
+  }
+  setCurrentTime() {
     const currentDate = new Date();
-    const currentDay = currentDate.getDay();
-    const currentHour = currentDate.getHours();
+    this.currentDay = currentDate.getDay();
+    this.currentHour = currentDate.getUTCHours() - 3;
 
-    this.openDays = weekDays.indexOf(currentDay) !== -1;
-    this.openHours= (weekHours[0] <= currentHour && currentHour < weekHours[1]);
+  }
+  setIsOpen() {
+    const openDays = this.weekDays.indexOf(this.currentDay) !== -1;
+    const openHours= (this.weekHours[0] <= this.currentHour && this.currentHour < this.weekHours[1]);
+    return openDays && openHours;
   }
   toggleCommercialAvailability() {
-    if (this.openDays && this.openHours)
+    if (this.setIsOpen())
       this.element.classList.add(this.htmlClass);
   }
   init() {
+    this.setCommercialAvailability();
+    this.setCurrentTime();
+    this.setIsOpen()
     if (this.element) {
       this.toggleCommercialAvailability();
     }
