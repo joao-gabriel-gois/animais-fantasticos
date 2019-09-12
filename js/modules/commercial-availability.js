1943cff411567fd749f361b3fd7a086003467dd0
stdnt-cl <stdnt-cl@mac.git>
2019-09-12 08:06:32 -0300

    refactoring commercial-availabilit - last function cheers

---
+++ js/modules/commercial-availability.js
@@ -0,0 +1,27 @@
+export default class CommercialAvailability {
+  constructor(element, htmlClass) {
+    this.element = document.querySelector(element);
+    this.htmlClass = htmlClass;
+
+    const weekDays = this.element.dataset.week.split(',').map(item => +item);
+    const weekHours = this.element.dataset.hour.split(',').map(Number);
+    const currentDate = new Date();
+    const currentDay = currentDate.getDay();
+    const currentHour = currentDate.getHours();
+
+    this.openDays = weekDays.indexOf(currentDay) !== -1;
+    this.openHours= (weekHours[0] <= currentHour && currentHour < weekHours[1]);
+  }
+  toggleCommercialAvailability() {
+    if (this.openDays && this.openHours) {
+      this.element.classList.add(this.htmlClass);
+    } else {
+      this.element.classList.remove(this.htmlClass);
+    }
+  }
+  init() {
+    if (this.element) {
+      this.toggleCommercialAvailability();
+    }
+  }
+}
